ca473070ce0b99db837043aa935cf7a397423664
JosuaCarl <josi.carl@uni-tuebingen.de>
2025-02-25 12:50:38 +0100

    Added Adapter to extract units from data

---
+++ app/bin/main/org/cpuinfofetcher/UnitsAdapter.groovy
@@ -0,0 +1,46 @@
+package org.cpuinfofetcher
+
+import org.dflib.DataFrame
+import org.dflib.Printers
+
+/**
+ * Adapts columns with units to be more uniform
+ */
+class UnitsAdapter {
+
+    DataFrame unitToColumnName(DataFrame df, Map<String, String[]> unit_mapping) {
+        List<String> old_col_names = df.getColumnsIndex().toArray()
+        // Define new column names with units
+        List<String> new_col_names = []
+        for (String col_name : old_col_names) {
+            List<String> units = unit_mapping.get(col_name)
+            if (units == null || col_name.endsWith("(${units.get(0)})")) {
+                new_col_names.add(col_name)
+            } else {
+                new_col_names.add("${col_name} (${units.get(0)})")
+            }
+        }
+
+        // Extract units from values
+        def new_df = DataFrame.byArrayRow(*new_col_names).appender()
+        for (int i = 0; i < df.height(); i++) {
+            List<String> units = unit_mapping.get(old_col_names.get(i))
+            List row = []
+            for (int j = 0; j < df.width(); j++) {
+                String value = df.get(j, i)
+                if (units != null) {
+                    for (String unit : units) {
+                        value = value.replaceAll("${units.get(0)}\$", '').trim()
+                    }
+                }
+                row.add(value)
+            }
+
+            new_df.append(*row)
+        }
+        new_df = new_df.toDataFrame()
+
+        return new_df
+    }
+
+}
